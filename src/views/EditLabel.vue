eaccaef17140516097332ab933c5a62dde368635
alicifes <635023574@qq.com>
2022-03-19 21:54:19 +0800

    完成对页面的改造

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <Icon class="rightIcon"/>
     </div>
     <div class="form-wrapper">
-      <FormItem field-name="标签名" placeholder="请输入标签名" :value="tag.name" @update:value="updateTag"/>
+      <FormItem field-name="标签名" placeholder="请输入标签名" :value="currentTag.name" @update:value="updateTag"/>
     </div>
     <div class="button-wrapper">
       <Button @click="remove">删除标签</Button>
@@ -25,28 +25,28 @@ import Button from '@/components/Button.vue';
 })
 export default class EditLabel extends Vue {
   //tag= store.findTag(this.$route.params.id);
-  get tag(){   //这里使用es6的get语法
+  get currentTag(){   //这里使用es6的get语法
     return this.$store.state.currentTag;
   }
   created() {
     this.$store.commit('fetchTags');
     const id = this.$route.params.id;
     this.$store.commit('setCurrentTag', id);
-    if (!this.tag) {
+    if (!this.currentTag) {
       this.$router.replace('/404');
     }
   }
 
   updateTag(name: string) {
-    if (this.tag) {
-      this.$store.commit('updateTag',{id:this.tag.id,name})
+    if (this.currentTag) {
+      this.$store.commit('updateTag',{id:this.currentTag.id,name})
       //store.updateTag(this.tag.id,name);
     }
   }
 
   remove() {
-    if (this.tag) {
-      this.$store.commit('removeTag',this.tag.id)
+    if (this.currentTag) {
+      this.$store.commit('removeTag',this.currentTag.id)
       //if(store.removeTag(this.tag.id)){
       //this.goBack();
       //     } else {
@@ -58,6 +58,10 @@ export default class EditLabel extends Vue {
       //  this.$router.back();
       // }
     }
+  }
+
+  goBack(){
+    this.$router.back()
   }
 }
 </script>
