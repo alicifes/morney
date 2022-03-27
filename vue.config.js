dc4a4941d3a04f6beddf0e06621da8b47dc8c7b0
alicifes <635023574@qq.com>
2022-03-27 16:13:44 +0800

    部署网站

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 
 const path = require('path')
 module.exports = {
+  publicPath:process.env.NODE_ENV==='production'?'/morney-website/':'/',
   lintOnSave: false,
   chainWebpack:config =>{
     const dir = path.resolve(__dirname,'src/assets/icons')  //首先确定当前处于哪个目录
