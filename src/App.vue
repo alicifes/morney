4c40d666d140e1d18eefa4a217702dd013d03b88
alicifes <635023574@qq.com>
2022-03-28 09:02:30 +0800

    修复主界面的问题

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <router-view></router-view>
   </div>
 </template>
@@ -14,5 +14,10 @@ body{
   -moz-osx-font-smoothing: grayscale;
   color: #333;
   background: #f5f5f5;
+}
+
+#app{
+  max-width: 500px;
+  margin: auto;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -20,19 +20,19 @@ new Vue({
 }).$mount('#app');
 
 window.onload = function (){
+  // if(document.documentElement.clientWidth>500){
+  //   window.alert("为了保证使用效果，请使用手机打开界面");
+  //   const img =document.createElement('img');
+  //   img.src='./prcode.png';
+  //   img.style.position = 'fixed';
+  //   img.style.top= '50%';
+  //   img.style.left = '50%';
+  //   img.style.transform='translate(-50%,-50%)'
+  //   img.style.boxShadow='0 0 10px rgba(0,0,0,0.3)'
+  //   document.body.append(img);
+  // }
  setTimeout(function(){
    window.scrollTo(0,100000);
  },0)
 }
 
-if(document.documentElement.clientWidth>500){
-  window.alert("为了保证使用效果，请使用手机打开界面");
-  const img =document.createElement('img');
-  img.src='./prcode.png';
-  img.style.position = 'fixed';
-  img.style.top= '50%';
-  img.style.left = '50%';
-  img.style.transform='translate(-50%,-50%)'
-  img.style.boxShadow='0 0 10px rgba(0,0,0,0.3)'
-  document.body.append(img);
-}
